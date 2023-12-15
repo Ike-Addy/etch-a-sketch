87d5b5536799f593e2ae5528bd4266b96f30c1b7
Ike Addy <ikaddy05@gmail.com>
2023-12-15 11:21:01 -0500

    Removed clear grid btn Added prompts to get user inputs Changed container size Changed cell size

--- scripts.js
+++ scripts.js
@@ -1,28 +1,33 @@
 const createGrid = document.getElementById('create-grid');
 const gridContainer = document.getElementById('grid-container');
-const clearGrid = document.getElementById('clear-grid');
 let cells = []; // Array to store created cells
 
 createGrid.addEventListener('click', () => {
+    let gridSize = prompt('How big would your space to be?', 16);
+    if (gridSize === '' || gridSize < 3 || gridSize > 100){
+        alert('Please enter a different number');
+    }
+
     cells = [];
     gridContainer.innerHTML = '';
 
-    for (let i = 0; i < 16 * 16; i++) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
         const cell = document.createElement('div');
         cell.className = 'grid';
-        cell.style.width = '30px';
-        cell.style.height = '30px';
-        cell.style.border = '0.5px solid black'
+        cell.style.width = '1rem';
+        cell.style.height = '1rem';
+        cell.style.border = '1px solid black'
         
         gridContainer.appendChild(cell);
         cells.push(cell);
-    }
-})
+    };
 
-clearGrid.addEventListener('click', () => {
-    cells = [];
-    gridContainer.innerHTML = '';
-})
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black'
+        });
+    });
+});
 
 
 
